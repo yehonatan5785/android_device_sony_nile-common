2d387df217f4c2f6eba435d57374dff3e79940b3
LineageOS-Special-Project <yehonatan@mail.ru>
2025-01-09 21:12:31 +0000

    nile-common: Remove deprecated FM radio components
    Purged legacy FM radio packages:
    - FM2 app
    - libqcomfm_jni (JNI interface)
    - qcom.fmradio (QCOM FM service)
    
    Rationale:
    1. Unused feature in modern devices
    2. Saves ~2.3MB in system image
    3. Reduces security surface area
    4. Eliminates unnecessary background service
    5. Part of ongoing debloat effort
    
    This removal aligns with current user needs where:
    - Internet streaming dominates audio
    - Hardware FM support is rarely utilized
    - Every KB matters in system partition

--- nile.mk
+++ nile.mk
@@ -201,12 +201,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
-
 # GPS
 PRODUCT_PACKAGES += \
     libwifi-hal-ctrl
